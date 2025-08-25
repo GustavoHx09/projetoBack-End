1535a5be0fccedeadf968934814f0611314178dc
Gustavo Henrique <gustavo.henrique091104@gmail.com>
2025-08-24 23:22:45 -0300

    move games on lists

--- src/main/java/com/myproject/treino/controllers/GameListController.java
+++ src/main/java/com/myproject/treino/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myproject.treino.dto.GameListDTO;
 import com.myproject.treino.dto.GameMinDTO;
+import com.myproject.treino.dto.ReplacementDTO;
 import com.myproject.treino.services.GameListService;
 import com.myproject.treino.services.GameService;
 
@@ -35,4 +38,8 @@ public class GameListController {
 		return result;
 	}
 
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+	}
 }
